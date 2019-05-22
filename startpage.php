f50a91246cf44026cbd5ce9c42860cd563fbb5d8
pluthin <pontus.lut@gmail.com>
2019-05-22 08:49:24 +0200

    added a button for startpage and some design

--- startpage.php
+++ startpage.php
@@ -6,12 +6,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/main.css">
-        <title>Book Upload</title>
+        <title>Book Upload startpage</title>
 </head>
 <body>
-    
-</body>
 
+        <h1>Book Consumer System</h1>
+
+        <section>
+                <a href="uploadpage.php"><button type="button" class="btn btn-lg">Lets go and upload</button>
+        </section>
+
+</body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
