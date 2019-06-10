aebd0d04398282dc703aa86bbd5d60198d135d67
pluthin <pontus.lut@gmail.com>
2019-06-10 20:55:56 +0200

    correct api info

--- includes/api.inc.php
+++ includes/api.inc.php
@@ -3,22 +3,26 @@
 class getAPi{
 
     //function to get information from the specific api. 
-    public function getBookInfo(){
-
-       
+    public function getBookInfo($table_name, $table_id){     
   
         //Api url 
-        $url = 'https://5ce8007d9f2c390014dba45e.mockapi.io/books/';
+        $url = 'https://5ce8007d9f2c390014dba45e.mockapi.io';
+        $table = "/" . $table_name;
+        $id =  "/" . $table_id;
 
-       
         $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_URL, $url.$id = "9789178034482");
+        curl_setopt($ch, CURLOPT_URL, $url . $table . $id);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
         $data = curl_exec($ch);
         curl_close($ch);
-        
-        return $data;
 
+        $obj = json_decode($data);
+        var_dump($obj);
+        $result = [];
+        foreach($obj as $key => $val) {
+            $result[$key] = $val;
+        }
+        return $result;
     }
 
 
@@ -55,16 +59,16 @@ class getAPi{
       //function to add new hard coded test values to new_books.csv
     public function fill_book($data){
 
-
-       foreach($data as $test){
+        
+       
         $book = [];
         //$book [] = ["ISBN", "Title", "Author"];
-        $book[0] = $data[1];
-        $book[1] = $data[2];
-        $book[2] = $data[3];
-        $book[3] = $data[4];
+        $book[0] = $data[0];
+        $book[1] = $data[1];
+        $book[2] = $data[4];
+        $book[3] = $data[6];
         return $book;
-        }
+        
        
     }
 
